72f81c28a8f31e21e5aaed603ea82da6bd5c2968
lgesfe03 <94594384+lgesfe03@users.noreply.github.com>
2023-04-06 12:17:15 +0800

    edit UI and move file hierarchy

--- find_sum_v2_OOP.py
+++ find_sum_v2_OOP.py
@@ -22,6 +22,7 @@ class ExcelReader(tk.Frame):
 
         self.choose_file_button = tk.Button(self, text="Choose file", command=self.choose_file)
         self.choose_file_button.pack(padx=20, pady=10)
+        self.first_choose = False
 
         #mark add
         # Create a dropdown menu to select a column
@@ -31,74 +32,83 @@ class ExcelReader(tk.Frame):
 
     def choose_file(self):
         filename = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
-        self.file_label.config(text="Chosen file: " + filename)
+        self.file_label.config(text="1.Chosen file: " + filename)
         self.data = pd.read_excel(filename)
         #mark add
-        # Get a list of column names from the DataFrame
-        self.column_names = list(self.data.columns)
-        self.column_dropdown = tk.OptionMenu(self, self.column_var, *self.column_names)
-        self.column_dropdown.pack(padx=20, pady=10)
-        #add last buttons
-        self.column_label = tk.Label(self, text="Type sum want to find")
-        self.column_label.pack(padx=20, pady=10)
+        if self.first_choose == False:
+            # Get a list of column names from the DataFrame
+            self.column_dropdown_text = tk.Label(self, text="2.choose column below")
+            self.column_dropdown_text.pack(padx=20, pady=10)
+            self.column_names = list(self.data.columns)
+            self.column_dropdown = tk.OptionMenu(self,  self.column_var, *self.column_names)
+            self.column_dropdown.pack(padx=20, pady=10)
+            #add last buttons
+            self.column_label = tk.Label(self, text="3.Type sum want to find")
+            self.column_label.pack(padx=20, pady=10)
 
-        self.taget_sum_entry = tk.Entry(self)
-        self.taget_sum_entry.pack(padx=20, pady=10)
+            self.taget_sum_entry = tk.Entry(self)
+            self.taget_sum_entry.pack(padx=20, pady=10)
 
-        self.read_button = tk.Button(self, text="Read column", command=self.read_column)
-        self.read_button.pack(padx=20, pady=10)
+            self.read_button = tk.Button(self, text="4.Read column", command=self.read_column)
+            self.read_button.pack(padx=20, pady=10)
+            self.first_choose = True
         
-
-    def subset_sum_indices(self,arr, target_sum): 
+    def subset_sum_indices(self,arr, target_sum):
         n = len(arr)
-        self.dp = [[None for j in range(target_sum+1)] for i in range(n+1)]
+        dp = [[None for j in range(target_sum+1)] for i in range(n+1)]
         for i in range(n+1):
-            self.dp[i][0] = []
-            # print("arr[" , i ,"]=",arr[i])
-            # if arr[i] == 'nan':
-            #     arr[i] = 0
+            dp[i][0] = []
         for i in range(1, n+1):
             for j in range(1, target_sum+1):
                 if arr[i-1] <= j:
-                    prev_subset = self.dp[i-1][j]
+                    prev_subset = dp[i-1][j]
                     if prev_subset is not None:
-                        self.dp[i][j] = prev_subset
+                        dp[i][j] = prev_subset
                     else:
-                        prev_subset = self.dp[i-1][j-arr[i-1]]
+                        #has force array index change into integer
+                        prev_subset = dp[i-1][int(j-arr[i-1])]
                         if prev_subset is not None:
-                            self.dp[i][j] = prev_subset + [i-1]
+                            dp[i][j] = prev_subset + [i-1]
                 else:
-                    self.dp[i][j] = self.dp[i-1][j]
-            return self.dp[n][target_sum]    
+                    dp[i][j] = dp[i-1][j]
+        return dp[n][target_sum]
 
     def read_column(self):
-        self.selected_column = self.column_var.get()
-        self.column_data = self.data[self.selected_column]
-        print("self.column_data=",self.column_data)
-        # print("type=",type(self.column_data))
-        length = len(self.column_data)
-        for i in range(length) :
-            print("self.column_data[" , i ,"]=",self.column_data[i])
-            if pd.isna(self.column_data[i]):
-                # np.isnan(self.column_data[i])
-                print(">>>>self.column_data[i] == nan")
-                self.column_data[i] = 0.0
-        print("**self.column_data=",self.column_data)
-        # print("**type=",type(self.column_data))
+        try:
+            self.selected_column = self.column_var.get()
+            self.column_data = self.data[self.selected_column]
+            print("self.column_data=\r\n",self.column_data)
+            # print("type=",type(self.column_data))
+            length = len(self.column_data)
+            # check data if 'nan' replace with 0
+            for i in range(length) :
+                # print("self.column_data[" , i ,"]=",self.column_data[i])
+                if pd.isna(self.column_data[i]):
+                    # np.isnan(self.column_data[i])
+                    print(">>>>self.column_data[i] == nan")
+                    self.column_data[i] = 0
+                    int(self.column_data[i])
+            # print("***\r\nself.column_data=",self.column_data,"\r\n***\r\n")
+            # print("**type=",type(self.column_data))
 
-        self.taget_sum = self.taget_sum_entry.get()
-        self.taget_sum = int(self.taget_sum)
-        print("self.taget_sum=",self.taget_sum)
-        print("type(self.taget_sum)=",type(self.taget_sum))
-        
-        result = self.subset_sum_indices(self.column_data, self.taget_sum)
-        if result is not None:
-            end_text = "Excel Subset found:", [self.column_data[i] for i in result]
-            self.output_info.config(text=  end_text)
-            print("Excel Subset found:", [self.column_data[i] for i in result])
-        else:
-            self.output_info.config(text="Excel No subset found")
-            print("Excel No subset found")
+            self.taget_sum = self.taget_sum_entry.get()
+            self.taget_sum = int(self.taget_sum)
+            print("self.taget_sum=",self.taget_sum)
+            # print("type(self.taget_sum)=",type(self.taget_sum))
+            
+            result = self.subset_sum_indices(self.column_data, self.taget_sum)
+            if result is not None:
+                end_text = "Excel found:", [int(self.column_data[i]) for i in result]
+                
+                self.output_info.config(text=  end_text)
+                print(end_text)
+            else:
+                end_text = "Excel NOT found"
+                self.output_info.config(text= end_text)
+                print(end_text)
+        except KeyError:
+            end_text = "***Choose Column!***"
+            self.output_info.config(text= end_text)
             
 
 
